878a2a061edd7c709b817323436e2d112a7cc311
Idain <luiscarlosholguinperez@outlook.com>
2021-02-02 23:34:37 -0400

    Single-player, Overworld, Audio, Text fixes
    All glitches documented in these aras have been fixed, except for the Entei's Pokédex cry glitch.

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -412,9 +412,7 @@ UsedSurfScript:
 
 	special UpdatePlayerSprite
 	special PlayMapMusic
-; step into the water (slow_step DIR, step_end)
 	special SurfStartStep
-	applymovement PLAYER, wMovementBuffer
 	end
 
 .stubbed_fn
