41c95eef2f490f5d060ce6d0c64cc96e9c5d166b
Idain <luiscarlosholguinperez@outlook.com>
2023-05-09 18:50:56 -0400

    Add warns to farcall macros

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -1,15 +1,19 @@
 ; Far calls to another bank
 
 MACRO farcall ; bank, address
+	assert !(\1 & $8000), "cannot `farcall \1` in RAM"
 	ld a, BANK(\1)
 	ld hl, \1
 	rst FarCall
+	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `farcall \1`"
 ENDM
 
 MACRO callfar ; address, bank
+	assert !(\1 & $8000), "cannot `callfar \1` in RAM"
 	ld hl, \1
 	ld a, BANK(\1)
 	rst FarCall
+	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `callfar \1`"
 ENDM
 
 MACRO homecall
