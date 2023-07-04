c0731cecf7f13029964ef50520b542ab5da0cd8d
Idain <luiscarlosholguinperez@outlook.com>
2023-07-04 18:45:43 -0400

    Indent with tabs, not spaces

--- audio/wave_tables.asm
+++ audio/wave_tables.asm
@@ -1,20 +1,20 @@
 WaveTables:
-    ; entries correspond to wCurWaveTable indexes
-    table_width 2, WaveTables
-    dw WaveSamples1
-    dw WaveSamples2
-    dw WaveSamples3
-    dw WaveSamples4
-    dw WaveSamples5
-    dw WaveSamples6
-    dw WaveSamples7
-    dw WaveSamples8
-    dw WaveSamples9
-    dw WaveSamples10
-    dw WaveSamples11
-    dw WaveSamples12
-    dw WaveSamples13
-    dw WaveSamples14
-    dw WaveSamples15
-    dw WaveSamples16
-    assert_table_length NUM_WAVETABLES
+	; entries correspond to wCurWaveTable indexes
+	table_width 2, WaveTables
+	dw WaveSamples1
+	dw WaveSamples2
+	dw WaveSamples3
+	dw WaveSamples4
+	dw WaveSamples5
+	dw WaveSamples6
+	dw WaveSamples7
+	dw WaveSamples8
+	dw WaveSamples9
+	dw WaveSamples10
+	dw WaveSamples11
+	dw WaveSamples12
+	dw WaveSamples13
+	dw WaveSamples14
+	dw WaveSamples15
+	dw WaveSamples16
+	assert_table_length NUM_WAVETABLES
