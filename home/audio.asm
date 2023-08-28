8bac051f90ef2dc1c61718d03b749575fdebe7db
Idain <luiscarlosholguinperez@outlook.com>
2023-08-28 06:32:18 -0400

    Slightly optimize WaitPlaySFX

--- home/audio.asm
+++ home/audio.asm
@@ -148,6 +148,9 @@ endr
 	pop hl
 	ret
 
+WaitPlaySFX::
+	call WaitSFX
+	; fallthrough
 PlaySFX::
 ; Play sound effect de.
 ; Sound effects are ordered by priority (highest to lowest)
@@ -185,10 +188,6 @@ PlaySFX::
 	pop de
 	pop hl
 	ret
-
-WaitPlaySFX::
-	call WaitSFX
-	jp PlaySFX
 
 WaitSFX::
 ; infinite loop until sfx is done playing
