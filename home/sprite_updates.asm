a2688bd457e107cc140de8144270e415642aea14
Idain <luiscarlosholguinperez@outlook.com>
2023-12-24 04:50:59 -0400

    Slightly optimize Enable/DisableSpriteUpdate functions

--- home/sprite_updates.asm
+++ home/sprite_updates.asm
@@ -1,19 +1,17 @@
 DisableSpriteUpdates::
 	xor a
+	ld [wSpriteUpdatesEnabled], a
 	ldh [hMapAnims], a
 	ld a, [wVramState]
 	res 0, a
 	ld [wVramState], a
-	xor a
-	ld [wSpriteUpdatesEnabled], a
 	ret
 
 EnableSpriteUpdates::
 	ld a, $1
 	ld [wSpriteUpdatesEnabled], a
+	ldh [hMapAnims], a
 	ld a, [wVramState]
 	set 0, a
 	ld [wVramState], a
-	ld a, $1
-	ldh [hMapAnims], a
 	ret
