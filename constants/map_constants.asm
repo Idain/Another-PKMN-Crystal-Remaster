df194fb0234b00427a2bc3492de5899ef8f322cf
Idain <luiscarlosholguinperez@outlook.com>
2024-01-02 03:06:07 -0400

    fix: Adjust Route 20's witdth and height

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -187,7 +187,7 @@ ENDM
 	map_const ROUTE_19_FUCHSIA_GATE,                        5,  4 ;  3
 	map_const SEAFOAM_GYM,                                  5,  4 ;  4
 	map_const ROUTE_19,                                    10, 18 ;  5
-	map_const ROUTE_20,                                    30,  9 ;  6
+	map_const ROUTE_20,                                    50, 13 ;  6
 	map_const ROUTE_21,                                    10, 18 ;  7
 	map_const CINNABAR_ISLAND,                             10,  9 ;  8
 	endgroup
